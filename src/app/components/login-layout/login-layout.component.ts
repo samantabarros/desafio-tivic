ce88e1c2eb03e3fbaa7c78d60d933ca6e58c3e30
samantabarros <samantadesenvolvedora@gmail.com>
2025-01-09 11:22:25 -0300

    feat: Estrutura da página de login

---
+++ src/app/components/login-layout/login-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-login-layout',
+  imports: [],
+  templateUrl: './login-layout.component.html',
+  styleUrl: './login-layout.component.scss'
+})
+export class LoginLayoutComponent {
+  @Input() btnLogin: string = "";
+}
